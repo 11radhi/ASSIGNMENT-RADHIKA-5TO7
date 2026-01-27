9c1c042385da3ede9dbb2e0bc19f44a874678afe
11radhi <radhikasurendar11@gmail.com>
2026-01-27 22:54:33 +0530

    update

---
+++ Day 27/task1.js
@@ -0,0 +1,26 @@
+// What is an event in JavaScript?
+
+The JavaScript event loop is a mechanism that allows JavaScript, a single-threaded language, 
+to handle multiple tasks like user interactions, API calls, and timers in a non-blocking way. 
+
+// Difference between inline events and addEventListener()
+
+Inline events attach JavaScript directly within HTML attributes (e.g., onclick="..."), 
+while the addEventListener() method is 
+a more modern, flexible approach that attaches 
+Event handlers entirely within JavaScript code
+
+// Write a mouse click event
+
+A mouse click event is typically handled using JavaScript in web development. The standard 
+way to write a mouse click event is by using the addEventListener() method on an element.
+
+// Detect a key press
+
+The simplest and most reliable way to detect a key press in 
+modern JavaScript is by using the **keydown** event listener with the **event.key** property
+
+// Explain event bubbling
+
+The simplest and most reliable way to detect a key press in modern JavaScript
+is by using the **keydown** event listener with the **event.key** property
